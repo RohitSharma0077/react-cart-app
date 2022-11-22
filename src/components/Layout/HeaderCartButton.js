ae151582005e4dc32471bb5016e28922a9362da4
rohitsharma0077 <rohits978699@gmail.com>
2022-11-22 12:14:04 +0530

    updating

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,23 +1,43 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../store/cart-context';
 
 const HeaderCartButton = props =>{
 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     // where onClick is a keyword and onClick in props.onClick would be any custom-name coming fom other component
 
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
     // const numberOfCartItems = cartCtx.items.length;
 
     // syntax : reduce(function, initial-value );
     const numberOfCartItems = cartCtx.items.reduce((curNumber, items) => {
         return curNumber + items.amount;
     }, 0 );
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+      if (items.length === 0) {
+        return;
+      }
+      setBtnIsHighlighted(true);
+  
+      const timer = setTimeout(() => {
+        setBtnIsHighlighted(false);
+      }, 300);
+  
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [items]);
     
       return (
-          <button className={classes.button} onClick={props.onClick}>  
+          <button className={btnClasses} onClick={props.onClick}>  
               <span className={classes.icon}>
                   <CartIcon />
               </span>
